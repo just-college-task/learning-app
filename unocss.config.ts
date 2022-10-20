908d1c316a4c6a3202a93e0047186007a28c1e60
fronz <z233@msn.com>
2022-10-20 17:42:03 +0800

    chore: unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -7,7 +7,15 @@ export default defineConfig({
     // https://github.com/MellowCo/unocss-preset-weapp
     presetWeapp({
       isH5: process.env.TARO_ENV === 'h5',
-      platform: 'taro'
+      platform: 'taro',
+      taroWebpack: 'webpack5',
+      designWidth: 640,
+      deviceRatio: {
+        640: 2.34 / 2,
+        750: 1,
+        828: 1.81 / 2,
+        375: 2 / 1
+      }
     })
   ],
   transformers: [
