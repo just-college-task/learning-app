1f141411fa6d4d5f8e3f686226b172cfceb0fbe9
fronz <z233@msn.com>
2022-10-22 16:57:24 +0800

    chore: change design width & add typography rules

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const config = {
   projectName: 'learning-app',
   date: '2022-10-17',
-  designWidth: 640,
+  designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
--- unocss.config.ts
+++ unocss.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       isH5: process.env.TARO_ENV === 'h5',
       platform: 'taro',
       taroWebpack: 'webpack5',
-      designWidth: 640,
+      designWidth: 750,
       deviceRatio: {
         640: 2.34 / 2,
         750: 1,
@@ -26,5 +26,35 @@ export default defineConfig({
     }),
 
     transformerClass()
+  ],
+  rules: [
+    [
+      'text-big-title',
+      {
+        'font-size': '40rpx',
+        'line-height': '72rpx'
+      }
+    ],
+    [
+      'text-description',
+      {
+        'font-size': '24rpx',
+        'line-height': '36rpx'
+      }
+    ],
+    [
+      'text-small-title',
+      {
+        'font-size': '36rpx',
+        'line-height': '56rpx'
+      }
+    ],
+    [
+      'text-normal',
+      {
+        'font-size': '32rpx',
+        'line-height': '48rpx'
+      }
+    ]
   ]
 })
