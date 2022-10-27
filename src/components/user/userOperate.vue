f8c3f6d7900042ba2baf3e769f2ce6a949084e91
fronz <z233@msn.com>
2022-10-27 23:24:34 +0800

    refactor(my): user operate

--- src/components/user/userOperate.vue
+++ src/components/user/userOperate.vue
@@ -1,37 +1,41 @@
 <script setup lang="ts">
-import Taro from '@tarojs/taro'
+import { Router } from 'tarojs-router-next'
 
-function toMyCourses() {
-  Taro.navigateTo({ url: '/pages/my-detail/myCourses' })
+interface MenuItem {
+  title: string
+  iconName: string
+  action: (...args: any) => any
 }
-function toManagerCourses() {
-  Taro.navigateTo({ url: '/pages/my-detail/managerCourses' })
-}
-function toCertificates() {
-  Taro.navigateTo({ url: '/pages/my-detail/certificate' })
-}
+
+const menuItems: MenuItem[] = [
+  {
+    title: '我的课程',
+    iconName: 'message',
+    action: () => Router.toMyCourses()
+  },
+  {
+    title: '我管理的课程',
+    iconName: 'people',
+    action: () => Router.toMyManageCourses()
+  },
+  {
+    title: '我的证书',
+    iconName: 'success',
+    action: () => Router.toMyCertificates()
+  }
+]
 </script>
 
 <template>
   <view class="body flex flex-col justify-center w-580px bg-white mt-45px px-5 rounded-24px">
-    <view class="flex w-full h-104px justify-center items-center" @click="toMyCourses">
-      <nut-icon name="message" size="20" class="mr-32px"></nut-icon>
+    <view
+      v-for="item in menuItems"
+      class="flex w-full h-104px justify-center items-center"
+      @click="item.action"
+    >
+      <nut-icon :name="item.iconName" size="20" class="mr-32px"></nut-icon>
       <view class="flex w-full h-full items-center justify-between border-b-1 border-b-gray-2">
-        <text>我的课程</text>
-        <view class="arr"></view>
-      </view>
-    </view>
-    <view class="flex w-full h-104px justify-center items-center" @click="toManagerCourses">
-      <nut-icon name="people" size="20" class="mr-32px"></nut-icon>
-      <view class="flex w-full h-full items-center justify-between border-b-1 border-b-gray-2">
-        <text>我管理的课程</text>
-        <view class="arr"></view>
-      </view>
-    </view>
-    <view class="flex w-full h-104px justify-center items-center" @click="toCertificates">
-      <nut-icon name="success" size="20" class="mr-32px"></nut-icon>
-      <view class="flex w-full h-full items-center justify-between">
-        <text>我的证书</text>
+        <text>{{ item.title }}</text>
         <view class="arr"></view>
       </view>
     </view>
