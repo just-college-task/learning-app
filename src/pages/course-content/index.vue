35feb9881acc1a8d1038afaed00bfbacc5a9ee72
lideming <me@yuuza.net>
2022-10-28 16:02:19 +0800

    feat: announcement

--- src/pages/course-content/index.vue
+++ src/pages/course-content/index.vue
@@ -4,25 +4,29 @@ import NoticesTabContent from './views/NoticesTabContent.vue'
 import SectionsTabContent from './views/SectionsTabContent.vue'
 import ExamsTabContent from './views/ExamsTabContent.vue'
 import { useRouter } from '@tarojs/taro'
+import { useCourseQuery } from '@/composables/courses'
 const activeTabIndex = ref(0)
 
 const router = useRouter()
 const courseId = Number(router.params.id!)
 console.info('course-content', courseId)
+
+const courseQuery = useCourseQuery(courseId)
+const course = courseQuery.data
 </script>
 
 <template>
   <view class="bg-primary flex flex-col min-h-screen course-content">
     <view class="h-248 box-border flex items-end px-5 py-3">
       <view>
-        <view class="text-small-title font-bold text-white"> 英汉修辞对比赏析 </view>
+        <view class="text-small-title font-bold text-white">{{ course?.name }}</view>
         <view class="text-description text-gray-2 mt-1"> 京东科技组件库 </view>
       </view>
     </view>
     <view class="grow rounded-t-5 overflow-hidden flex flex-col">
       <nut-tabs v-model="activeTabIndex" class="grow flex flex-col">
         <nut-tabpane title="公告" class="left-0 absolute border-t-2 border-gray-2 bg-background">
-          <NoticesTabContent />
+          <NoticesTabContent :courseId="courseId" />
         </nut-tabpane>
         <nut-tabpane title="章节列表" class="left-[100%] absolute border-t-2 border-gray-2">
           <SectionsTabContent />
--- src/pages/course-content/views/NoticesTabContent.vue
+++ src/pages/course-content/views/NoticesTabContent.vue
@@ -1,9 +1,21 @@
 <template>
   <view class="w-full bg-white rounded-lg px-4 py-3 box-border">
-    <view v-for="i in 3">
-      <view class="text-normal text-gray-9">开课通知</view>
-      <view class="text-description mt-1 text-gray-4">2022-10-20</view>
-      <nut-divider v-if="i < 3" class="text-gray-200 my-3" />
+    <view v-for="(a, i) in announcement?.dataList ?? []">
+      <view class="text-normal text-gray-9">{{ a.title }}</view>
+      <view class="text-description mt-1 text-gray-4">{{
+        new Date(a.Time).toLocaleDateString()
+      }}</view>
+      <nut-divider v-if="i < announcement?.dataList.length!" class="text-gray-200 my-3" />
     </view>
   </view>
 </template>
+<script setup lang="ts">
+import { useAnnouncementsQuery } from '@/composables/courses'
+
+const props = defineProps<{
+  courseId: number
+}>()
+
+const announcementQuery = useAnnouncementsQuery(props.courseId)
+const announcement = announcementQuery.data
+</script>
