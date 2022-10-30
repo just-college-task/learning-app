fd3a10c7de1de737046a69a6e6f152b12b5f6047
Lorde <rioaformyking@gmail.com>
2022-10-30 09:28:02 +0800

    feat: add About page

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <view class="page-wrapper">About</view>
+  <view class="page-wrapper flex flex-col items-center w-full h-100vh text-center">
+    <view class="text-xl font-bold mt-5">About</view>
+    <view class="mt-30vh">
+      <view>小程序使用 Taro Vue3 + NutUI 作为前端</view>
+      <view>使用SpringBoot + Mysql + SpringSecurity 作为后端</view>
+      <view>项目仍未完善，持续在GitHub推进中~</view>
+    </view>
+  </view>
 </template>
