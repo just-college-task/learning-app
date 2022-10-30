8f1ac80871b18ed702c57bdb97c0dce99472a946
Lorde <rioaformyking@gmail.com>
2022-10-30 13:09:03 +0800

    feat: add certificationsItem router

--- src/components/CertificationItem.vue
+++ src/components/CertificationItem.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import { Course } from 'types/api'
 import { toRefs } from 'vue'
+import { Router } from 'tarojs-router-next'
+
+function handleDetailClicked() {
+  Router.navigate({
+    url: `/pages/course-detail/index?id=${course.value.courseId}`
+  })
+}
 
 const DEFAULT_COVER_SRC = `https://img-ph-mirror.nosdn.127.net/tYhzuDVilzlDOo2bEyH_Qg==/6608226511143817333.jpg?imageView&quality=100&thumbnail=230y130`
 
@@ -9,7 +16,7 @@ const { course } = toRefs(props)
 </script>
 
 <template>
-  <view class="flex gap-3">
+  <view class="flex gap-3" @click="handleDetailClicked">
     <img class="h-144 w-240 object-cover" :src="course.course.poster ?? DEFAULT_COVER_SRC" />
     <view class="flex-1 flex flex-col justify-between">
       <view class="flex flex-col gap-1">
